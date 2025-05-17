63db64b148c65d54de228f9d69e613d74b32fcda
ProgramWind <121439140+ProgramWind@users.noreply.github.com>
2025-05-17 15:58:59 +0530

    User Related Features

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import Register from './Header/Register'
+import Login from './Header/Login'
+import Contact from './Header/Contact'
+import About from './Header/About'
+import Home from './Header/Home'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Header from './Header/Header'
+import Dashboard from './DashBoard/Dashboard'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        {/* <h2>Header</h2>    */}
+        <BrowserRouter>
+
+        <Header/>
+
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/dashboard/*' element={<Dashboard/>}></Route>
+        <Route path='/Logout' element={<Home/>}></Route>
+
+      </Routes>
+
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
